4a6a6ff8eec7425626aa8e4692747bf90d4c01d0
Sureh <suresh.sharma@varahisoft.com>
2025-08-27 17:55:14 +0530

    add data file in about page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "47c85b4886007eb9"; }
+/******/ 		__webpack_require__.h = function() { return "50acfb789850b7c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
